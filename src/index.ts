9d9a330c36eb6377b4ad91e45d721d34d2c3278d
DaeHyun Lee <dhlee2@autocrypt.io>
2021-11-19 15:45:30 +0900

    conflict test

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 // interface Human {
 //   name: string;
 //   age: number;
-//   test: string;
+//   qwe: string;
 // }
 
 class Human {
   public name: string;
   public age: number;
-  public test: string;
-  constructor(name: string, age: number, test?: string) {
+  public qwe: string;
+  constructor(name: string, age: number, qwe?: string) {
     this.name = name;
     this.age = age;
-    this.test = test;
+    this.qwe = qwe;
   }
 }
 
-// const me = { name: "이대현", age: 23, test: "male" };
+// const me = { name: "이대현", age: 23, qwe: "male" };
 const me = new Human("이대현", 23, "male");
 
 const sayHi = (me: Human): string => {
-  return `Hello ${me.name}, you are ${me.age}, you are a ${me.test}`;
+  return `Hello ${me.name}, you are ${me.age}, you are a ${me.qwe}`;
 };
 
 console.log(sayHi(me));
-// test branch merge test
+// qwe branch merge qwe
 // gogogogogogo
 export {};
