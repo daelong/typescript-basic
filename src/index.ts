13b99eebab505ab7b1094321dc72ef2e13dac126
DaeHyun Lee <dhlee2@autocrypt.io>
2021-11-19 15:42:15 +0900

    diff test

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 // interface Human {
 //   name: string;
 //   age: number;
-//   test: string;
+//   gender: string;
 // }
 
 class Human {
   public name: string;
   public age: number;
-  public test: string;
-  constructor(name: string, age: number, test?: string) {
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
     this.name = name;
     this.age = age;
-    this.test = test;
+    this.gender = gender;
   }
 }
 
-// const me = { name: "이대현", age: 23, test: "male" };
+// const me = { name: "이대현", age: 23, gender: "male" };
 const me = new Human("이대현", 23, "male");
 
 const sayHi = (me: Human): string => {
-  return `Hello ${me.name}, you are ${me.age}, you are a ${me.test}`;
+  return `Hello ${me.name}, you are ${me.age}, you are a ${me.gender}`;
 };
 
 console.log(sayHi(me));
-// test branch merge test
+// gender branch merge gender
 // gogogogogogo
 export {};
